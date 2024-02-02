8228dbdedc42933d523024457efc8500332949bd
Terry Amato <terence.amato@terumobct.com>
2024-02-02 16:23:31 -0700

    adsf

---
+++ pyprint.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import sys
+
+print("say hello to the world, arg entries: ",sys.argv[1:])
+
+#command - #script {adf} {python -m pyprint "*ta[], $ta[%*]"}; #var;
+
+# input is: #VARIABLE {ta} {{blah}{{mons2}{}{mson}{}}}
+# output looks like
+# ['\x7Bblah\x7D, \x7B\x7Bmons2\x7D\x7B\x7D\x7Bmson\x7D\x7B\x7D\x7D']}
+# inside of tintin++
