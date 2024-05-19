57e8866b97065615a8f9fb68011bc222f9259206
Terry Amato <terence.amato@gmail.com>
2024-05-18 19:55:32 -0600

    removal

--- lua/tests.lua
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/lua
-
---https://wiki.mudlet.org/w/Manual:Advanced_Lua#Regex_in_Lua
-
-local num = "2"
-if type(num) == "string" then print("just a string") end
-if type(num+0) == "number" then print("it is now a number") end
