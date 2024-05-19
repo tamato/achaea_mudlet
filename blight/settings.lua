6a05422b449bf82e91ecf8a5c03390d038a857ef
Terry Amato <terence.amato@gmail.com>
2024-05-18 20:25:12 -0600

    clear out blight

--- blight/settings.lua
+++
@@ -1,4 +0,0 @@
-return {
-   PASSWD = 'achaeapassword'
-}
-
--- blight/targeting.lua
+++
@@ -1,7 +0,0 @@
--- Targetting scripts
-
-
--- targets = {''}
--- targets = store.disk_read('targets')
--- store.disk_write('targets', json.encode(targets))
-
